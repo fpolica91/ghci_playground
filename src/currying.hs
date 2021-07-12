2ab8840315c0db21173d5306bed26e11466fd33e
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-07-11 23:57:34 -0400

    R

--- src/currying.hs
+++ src/currying.hs
@@ -1,6 +1,11 @@
 divides :: Int -> Int -> Bool
 divides a b = mod b a == 0
 
+-- flips values
+inverseDivides = flip divides
+
+factoryOfTwelve = (flip divides) 12
+
 both True True = True
 both _ _ = False
 
