0652b55142b30507e1d1448795043bff9f3331f7
fabricio policarpo <fabriciopolicarpo0@gmail.com>
2021-07-13 21:13:24 -0400

    compositon operator

--- src/HOC.hs
+++ src/HOC.hs
@@ -3,6 +3,19 @@ applyTwice fn number = fn (fn number)
 
 double number = number * 2
 
+next x = x + 1
+
 quadruple = applyTwice double
 
 timesSixteen = applyTwice quadruple
+
+-- NOTE  composition operator ., which combines two functions @compose
+compose = double . next
+
+curryISC fn i s = fn (i, s)
+
+uncurryISC fn (i, s) = fn i s
+
+-- NOTE takes a function taking integer and string arguments
+-- returns a function taking string and integer arguments.
+flipISC fn s i = fn i s
